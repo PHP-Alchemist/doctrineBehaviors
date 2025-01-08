c37f6485fafdef64a55df67f4ca9213a490bac6a
Micah Breedlove <druid628@gmail.com>
2025-01-07 22:23:01 -0500

    adds missing use statement

--- src/PHPAlchemistDoctrineBehaviorsBundle.php
+++ src/PHPAlchemistDoctrineBehaviorsBundle.php
@@ -4,11 +4,16 @@ namespace PHPAlchemist\DoctrineBehaviors;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 
 class PHPAlchemistDoctrineBehaviorsBundle extends AbstractBundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder) : void
+    public function loadExtension(
+        array                 $config,
+        ContainerConfigurator $container,
+        ContainerBuilder      $builder
+    ) : void
     {
         $container->import('../config/services.yaml');
     }
