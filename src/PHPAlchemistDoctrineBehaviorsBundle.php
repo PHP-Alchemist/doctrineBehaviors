1fe571e6a26795a8b3a9644ff6bd6e7be9de383b
Micah Breedlove <druid628@gmail.com>
2025-01-07 22:14:42 -0500

    Converted to Symfony Bundle

---
+++ src/PHPAlchemistDoctrineBehaviorsBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PHPAlchemist\DoctrineBehaviors;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+
+class PHPAlchemistDoctrineBehaviorsBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder) : void
+    {
+        $container->import('../config/services.yaml');
+    }
+}
